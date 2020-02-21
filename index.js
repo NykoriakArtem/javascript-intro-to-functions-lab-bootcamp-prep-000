b79a6dce716f1e8b081421745d82eec9046a06bc
Artem Nykoriak <artemnykoriak@gmail.com>
2020-02-21 04:10:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,6 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase())
+  string.toLowerCase()
    return  "I can't hear you!";
-   
-   else if (string.toUpperCase())
-   return "YES INDEED!";
 }
